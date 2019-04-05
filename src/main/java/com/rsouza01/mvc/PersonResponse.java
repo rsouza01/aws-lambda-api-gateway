2086e18101fabf5408f47aa5dccc779e20fdc792
Rodrigo Souza <rsouza01@gmail.com>
2019-04-04 23:04:19 -0300

    First commit

---
+++ src/main/java/com/rsouza01/mvc/PersonResponse.java
@@ -0,0 +1,16 @@
+package com.rsouza01.mvc;
+
+public class PersonResponse {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
